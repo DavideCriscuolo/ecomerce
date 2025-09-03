143b37d514a7fd183c438a94d0733a35706f7512
DavideCriscuolo <davide.alcatel@gmail.com>
2025-09-03 13:25:25 +0200

    implementata card sul prodotto singolo

--- src/components/MainSingoloProd.jsx
+++ src/components/MainSingoloProd.jsx
@@ -21,11 +21,68 @@ export default function MainSingoloProd() {
           <h2 className="text-center my-5">Prodotto singolo</h2>
           <div className="row">
             <div className="col">
-              <div className="card h-100">
-                <div className="card-body">
-                  <img className="card-img-top" src={prodotto.img} alt="" />
-                  <h5 className="card-title">{prodotto.nome}</h5>
-                  <p className="card-text">{prodotto.descrizione}</p>
+              <div className="card h-100 mb-3">
+                <div className="row g-0">
+                  <div className="col-md-4 p-3">
+                    <img
+                      src={prodotto.img}
+                      className="img-fluid rounded-start"
+                      alt="Card title"
+                    />
+                  </div>
+                  <div className="col-md-8">
+                    <div className="card-body">
+                      <h5 className="card-title">{prodotto.nome}</h5>
+                      <p className="card-text">{prodotto.descrizione}</p>
+                      <div>
+                        <ul className="list-unstyled list-group list-group-flush ">
+                          <li className="list-group-item"> {prodotto.case}</li>
+                          <li className="list-group-item">
+                            {" "}
+                            {prodotto.formato_case}
+                          </li>
+                          <li className="list-group-item">
+                            {" "}
+                            {prodotto.gb_ram}GB
+                          </li>
+                          <li className="list-group-item"> {prodotto.ram}</li>
+                          <li className="list-group-item">
+                            {" "}
+                            {prodotto.processore}
+                          </li>
+                          <li className="list-group-item">
+                            {" "}
+                            {prodotto.dissipatore}
+                          </li>
+                          <li className="list-group-item"> {prodotto.mobo}</li>
+                          <li className="list-group-item">
+                            {" "}
+                            {prodotto.scheda_video}
+                          </li>
+                          <li className="list-group-item">
+                            {" "}
+                            {prodotto.gb_vram}GB Vram
+                          </li>
+                          <li className="list-group-item">
+                            {" "}
+                            {prodotto.alimentatore}
+                          </li>
+                          <li className="list-group-item">
+                            {" "}
+                            {prodotto.archiviazione}{" "}
+                          </li>
+                          <li className="list-group-item">
+                            {" "}
+                            {prodotto.gb_archiviazione}GB
+                          </li>
+                          <li className="list-group-item">
+                            {" "}
+                            {prodotto.ventole}
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
